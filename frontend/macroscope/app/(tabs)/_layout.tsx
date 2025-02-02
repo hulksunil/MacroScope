2e9a06949fd576548c596fc812fb9d8a4a632cef
AGBellerive <AGBellerive@gmail.com>
2025-02-01 20:56:26 -0500

    Pushing a half baked landing page

--- frontend/macroscope/app/(tabs)/_layout.tsx
+++ frontend/macroscope/app/(tabs)/_layout.tsx
@@ -40,6 +40,14 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      
+      <Tabs.Screen
+        name="landingpage"
+        options={{
+          title: 'Landing Page',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
